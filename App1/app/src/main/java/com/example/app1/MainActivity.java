bc6576e36b194606a1dd0f1d9fc619cb81064796
Infotech Researcher <labrt.umm@gmail.com>
2020-02-11 10:02:41 +0700

    tambah fitur anyar

--- App1/app/src/main/java/com/example/app1/MainActivity.java
+++ App1/app/src/main/java/com/example/app1/MainActivity.java
@@ -9,14 +9,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener
-{
-
-    private EditText edtWidth;
-    private EditText edtHeight;
-    private EditText edtLength;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText edtWidth, edtHeight, edtLength;
     private Button btnCalculate;
     private TextView tvResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,26 +24,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         edtLength = findViewById(R.id.edt_length);
         btnCalculate = findViewById(R.id.btn_calculate);
         tvResult = findViewById(R.id.tv_result);
+
+        btnCalculate.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View view) {
-        if(view.getId()==R.id.btn_calculate){
-            String inputLength  = edtLength.getText().toString().trim();
-            String inputWidth  = edtWidth.getText().toString().trim();
-            String inputHeight  = edtHeight.getText().toString().trim();
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_calculate) {
+            String inputLength = edtLength.getText().toString().trim();
+            String inputWidth = edtWidth.getText().toString().trim();
+            String inputHeight = edtHeight.getText().toString().trim();
 
             boolean isEmptyFields = false;
             boolean isInvalidDouble = false;
 
-            if(TextUtils.isEmpty(inputLength)){
+            if (TextUtils.isEmpty(inputLength)) {
                 isEmptyFields = true;
-                edtLength.setError("Tidak boleh kosong");
+                edtLength.setError("Field ini tidak boleh kosong");
             }
-            if(TextUtils.isEmpty(inputLength)){
+
+            if (TextUtils.isEmpty(inputWidth)) {
                 isEmptyFields = true;
-                edtLength.setError("Tidak boleh kosong");
+                edtWidth.setError("Field ini tidak boleh kosong");
             }
+
+            if (TextUtils.isEmpty(inputHeight)) {
+                isEmptyFields = true;
+                edtHeight.setError("Field ini tidak boleh kosong");
+            }
+
+            Double length = toDouble(inputLength);
+            Double width = toDouble(inputWidth);
+            Double height = toDouble(inputHeight);
+
+
+            if (length == null || length >100 ) {
+                isInvalidDouble = true;
+                edtLength.setError("Field ini harus berupa nomer yang valid");
+            }
+
+            if (width == null || width >100) {
+                isInvalidDouble = true;
+                edtWidth.setError("Field ini harus berupa nomer yang valid");
+            }
+
+            if (height == null || height >100) {
+                isInvalidDouble = true;
+                edtHeight.setError("Field ini harus berupa nomer yang valid");
+            }
+
+            if (!isEmptyFields && !isInvalidDouble) {
+                double volume = length * width * height;
+                if(volume > 500){
+                    tvResult.setText("Hasil Terlalu Banyak Bos");
+                }else {
+                    tvResult.setText(String.valueOf(volume));
+                }
+            }
+        }
+    }
+
+    private Double toDouble(String str) {
+        try {
+            return Double.valueOf(str);
+        } catch (NumberFormatException e) {
+            return null;
         }
     }
 }
