d4fb50fba66a229e60e7fe0cc8635dacfbb7e789
Infotech Researcher <labrt.umm@gmail.com>
2020-04-30 10:17:07 +0700

    push Modul 5 v 0.1

--- mymaps.app/app/src/main/java/com/example/mymaps/DirectionsJSONParser.java
+++ mymaps.app/app/src/main/java/com/example/mymaps/DirectionsJSONParser.java
@@ -71,20 +71,20 @@ public class DirectionsJSONParser {
             int b, shift = 0, result = 0;
             do {
                 b = encoded.charAt(index++) - 63;
-                result |= (b &amp; 0x1f) << shift;
+                result |= (b & 0x1f) << shift;
                 shift += 5;
             } while (b >= 0x20);
-            int dlat = ((result &amp; 1) != 0 ? ~(result >> 1) : (result >> 1));
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
             lat += dlat;
 
             shift = 0;
             result = 0;
             do {
                 b = encoded.charAt(index++) - 63;
-                result |= (b &; 0x1f) << shift;
+                result |= (b & 0x1f) << shift;
                 shift += 5;
             } while (b >= 0x20);
-            int dlng = ((result &amp; 1) != 0 ? ~(result >> 1) : (result >> 1));
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
             lng += dlng;
 
             LatLng p = new LatLng((((double) lat / 1E5)),
